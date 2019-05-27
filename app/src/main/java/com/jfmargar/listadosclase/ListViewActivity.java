51ebae2d6f7f18ded6de51e6dc515d97f58ec3ca
Julián Marqués García <jfmargar@gmail.com>
2019-05-27 10:50:08 +0200

    Inicial

--- app/src/main/java/com/jfmargar/listadosclase/ListViewActivity.java
+++ app/src/main/java/com/jfmargar/listadosclase/ListViewActivity.java
@@ -2,12 +2,35 @@ package com.jfmargar.listadosclase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListViewActivity extends AppCompatActivity {
+
+    ListView lvDatos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        lvDatos = findViewById(R.id.lvDatos);
+
+        ArrayList<String> datos = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            datos.add("Dato " + i);
+        }
+
+        ArrayAdapter<String> adapter = new
+                ArrayAdapter<>(
+                ListViewActivity.this,
+                android.R.layout.simple_list_item_1,
+                datos);
+
+        lvDatos.setAdapter(adapter);
+
     }
 }
